bbdfc2f80fd69ecc3bf1a3b746cee074a6f9f016
Bibek Lamichhane <lamichhane.kebib@gmail.com>
2025-12-06 15:53:31 +0545

    feat: add app-ads.txt page
    Added a new React component for the app-ads.txt page, which displays the Google AdSense publisher ID and other relevant information. This is necessary for adhering to the App-ads.txt specification, which helps improve transparency and trust in the digital advertising ecosystem.

---
+++ pages/app-ads.txt.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppAdsPage = () => {
+  return (
+    <div>
+     <p>google.com, pub-1130382147297041, DIRECT, f08c47fec0942fa0
+</p>
+    </div>
+  );
+};
+
+export default AppAdsPage;
